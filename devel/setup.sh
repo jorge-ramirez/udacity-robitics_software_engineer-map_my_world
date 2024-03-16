706224935fb1d593a9eec8a643c127080313e45a
Jorge Ramirez <jorge.a.ramirez@mac.com>
2024-03-16 15:49:20 -0700

    updated for Map My World project

--- devel/setup.sh
+++ devel/setup.sh
@@ -11,7 +11,7 @@
 
 # since this file is sourced either use the provided _CATKIN_SETUP_DIR
 # or fall back to the destination set at configure time
-: ${_CATKIN_SETUP_DIR:=/home/robond/Documents/udacity-robitics_software_engineer-where_am_i/devel}
+: ${_CATKIN_SETUP_DIR:=/home/robond/Documents/udacity-robitics_software_engineer-map_my_world/devel}
 _SETUP_UTIL="$_CATKIN_SETUP_DIR/_setup_util.py"
 unset _CATKIN_SETUP_DIR
 
--- devel/share/ball_chaser/cmake/ball_chaser-msg-extras.cmake
+++ devel/share/ball_chaser/cmake/ball_chaser-msg-extras.cmake
@@ -1,2 +1,2 @@
 set(ball_chaser_MESSAGE_FILES "")
-set(ball_chaser_SERVICE_FILES "/home/robond/Documents/udacity-robitics_software_engineer-where_am_i/src/ball_chaser/srv/DriveToTarget.srv")
+set(ball_chaser_SERVICE_FILES "/home/robond/Documents/udacity-robitics_software_engineer-map_my_world/src/ball_chaser/srv/DriveToTarget.srv")
